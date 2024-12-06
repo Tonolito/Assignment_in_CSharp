bb22fd92c6b867a22c470762748bd68bf7a6b714
Tonlito <Antoniow4@hotmail.com>
2024-12-06 12:06:26 +0100

    Add project files.

---
+++ Presentation_MAUIBlazor/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Presentation_MAUIBlazor
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
